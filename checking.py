9e69bd3b30122205bd3c003840348296388d647f
coder0822 <reshmasf22@gmail.com>
2024-09-15 10:45:11 +0530

    L5

---
+++ checking.py
@@ -0,0 +1,8 @@
+i = int(input("Enter the number: "))
+if( i < 15 ):
+  print("i is smaller than number 15")
+  print ("i'm in if Block")
+else:
+  print("i is greater than 15")
+  print("i'm in else Block")
+  print("i'm not in if and not in else Block")
